fed2c662d241be7d9176ce7be54f4ede56408d7c
ccnncytutf-pixel <ccnncytutf@gmail.com>
2026-07-20 15:14:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Бот запущен")
+import telebot
+
+TOKEN = "8641592656:AAFfE4luC7BEdIBZvopcyaWscaZ33wamxTg"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я запущен 🤖")
+
+@bot.message_handler(func=lambda message: True)
+def answer(message):
+    bot.send_message(message.chat.id, "Ты написал: " + message.text)
+
+bot.infinity_polling()
